3f15cb426dc4441406c7dd2aa34df8885c0ed6d9
Tejas Hedly <tejas.hedly@kochgs.com>
2022-05-13 13:34:16 +0530

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup
+
+setup(
+    name='pythreading-wrapper',
+    version='0.1.0',
+    description='A threading wrapper which allows running multiple functions in parallel',
+    url='https://github.com/thedly/pythreading-wrapper',
+    author='Tejas Hedly',
+    author_email='tejas.hedly@gmail.com',
+    license='GNU GENERAL PUBLIC LICENSE',
+    packages=['pythreading-wrapper'],
+    install_requires=[],
+
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: GNU GENERAL PUBLIC LICENSE',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.1',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+)
