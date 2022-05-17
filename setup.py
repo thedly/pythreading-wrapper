0427c756f68a02b6a499474328a1d604438a3ae4
Tejas Hedly <tejas.hedly@kochgs.com>
2022-05-17 15:46:08 +0530

    Updated project name

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name='pythreading-wrapper',
+    name='thread_wrapper',
     version='0.0.1',
     description='A threading wrapper which allows running multiple functions in parallel',
     url='https://github.com/thedly/pythreading-wrapper',
