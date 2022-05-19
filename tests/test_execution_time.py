d172d59a9b94f305cf303b0f23446886faa87958
Tejas Hedly <tejas.hedly@kochgs.com>
2022-05-19 21:16:02 +0530

    Updated test files

--- tests/test_execution_time.py
+++ tests/test_execution_time.py
@@ -2,7 +2,7 @@ import time
 
 from pytest import fixture
 
-from ThreadingWrapper.concurrency import FunctionArgs, MultiThreading
+from ..ThreadingWrapper.concurrency import FunctionArgs, MultiThreading
 
 data = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
